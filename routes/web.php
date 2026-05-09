8fe0b596ede5d9d1e8f8d585703657551340f83c
keuysia <keyykesyaa@gmail.com>
2026-05-09 19:27:22 +0700

    init

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,24 @@
 <?php
-
+use App\Http\Controllers\FakultasController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/fakultas', function () {
-    return view('fakultas.list-fakultas');
-});
+Route::resource('/fakultas', FakultasController::class);
+// Route::get('/fakultas', function () {
+//     return view('fakultas.list-fakultas');
+// });
 
-Route::get('/edit', function () {
-    return view('fakultas.edit-fakultas');
-});
+// Route::get('/edit', function () {
+//     return view('fakultas.edit-fakultas');
+// });
 
-Route::get('/detail', function () {
-    return view('fakultas.detail-fakultas');
-});
+// Route::get('/detail', function () {
+//     return view('fakultas.detail-fakultas');
+// });
+
+// Route::get('/add', function () {
+//     return view('fakultas.add-fakultas');
+// });
