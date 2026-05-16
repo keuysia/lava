28f93c7d4e4b53eb3538cb564e08a420ada42739
keuysia <keyykesyaa@gmail.com>
2026-05-16 20:36:27 +0700

    main

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -53,6 +53,14 @@
     </div>
 </nav>
 </div>
+<main>
+  
+    @if (session('success'))
+      <div class="alert alert-success">
+          {{ session('success') }}
+    </div>
+    @endif
+</main>
     {{ $slot }}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
   </body>
